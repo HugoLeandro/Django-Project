efa98159794bd7330f549619ad9a7d930b3e2861
siteflask1 <leandrolimahugo@gmail.com>
2024-01-31 11:58:07 -0300

    update

--- openflix/settings.py
+++ openflix/settings.py
@@ -31,7 +31,7 @@ else:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["openflix-django.up.railway.app", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["*", "localhost", "127.0.0.1"]
 
 
 # Application definition
