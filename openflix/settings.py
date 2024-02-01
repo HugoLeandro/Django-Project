70dba073bb4113b81697e97e6e6e1d97eadb29a7
siteflask1 <leandrolimahugo@gmail.com>
2024-02-01 09:13:06 -0300

    update

--- openflix/settings.py
+++ openflix/settings.py
@@ -24,14 +24,14 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 TOKEN_CSRF = os.getenv('TOKEN_CSRF')
 if TOKEN_CSRF:
     SECRET_KEY = TOKEN_CSRF
-    CSRF_TRUSTED_ORIGINS = [""]
+    CSRF_TRUSTED_ORIGINS = ["https://openflix-django.up.railway.app"]
 else:
     SECRET_KEY = "django-insecure-(k9vb@k!w-v2%h+yr+(zuzr_q63@p!$-cid0o#+$6_%#w0p4fpw)"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["openflix-django.up.railway.app", "localhost", "127.0.0.1"]
 
 
 # Application definition
